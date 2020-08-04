9c79e443ec019b2950c0545dff6ca9a08abbfd6a
Yusuf Can <67974607+p4rss@users.noreply.github.com>
2020-08-04 22:56:16 +0300

    Update base64.py

--- base64.py
+++ base64.py
@@ -5,16 +5,8 @@ def base64_encode(sayi,yazi):
         string = pybase64.b64encode(string.encode(encoding='utf-8', errors='strict')).decode(encoding="utf-8")
     print(string)
 def base64_decode(sayi,yazi):
-    string = yazi
+    string = yazi + "=="
     for i in range(sayi):
         string  = pybase64.b64decode(string)
     out_string = string.decode("UTF-8")
     print(out_string)
-
-
-
-
-
-
-
-
